c2cfab402f6f51a61f1033adb62ef75869bdf1d3
lingdian <lingdian@meili-inc.com>
2017-08-25 23:11:48 +0800

    Initial Commit

---
+++ MyIOSApp4Tests/MyIOSApp4Tests.m
@@ -0,0 +1,39 @@
+//
+//  MyIOSApp4Tests.m
+//  MyIOSApp4Tests
+//
+//  Created by lingdian on 17/8/25.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyIOSApp4Tests : XCTestCase
+
+@end
+
+@implementation MyIOSApp4Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
